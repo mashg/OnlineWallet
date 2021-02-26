0f949ebf955cb24e47f511371e790cba4e8d19ce
AndryKovalenko <andry.kovalenko@gmail.com>
2021-02-26 09:40:15 +0200

    Comment Login

--- src/main/java/com/example/demo/config/JWTSecurityConfig.java
+++ src/main/java/com/example/demo/config/JWTSecurityConfig.java
@@ -5,16 +5,16 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-@Configuration
-public class JWTSecurityConfig extends WebSecurityConfigurerAdapter {
-
-	 @Override
-	    protected void configure(HttpSecurity http) throws Exception {
-	        http
-	          .authorizeRequests(authz -> authz
-	            .antMatchers(HttpMethod.GET, "/user/**").hasAuthority("SCOPE_read")
-	            .antMatchers(HttpMethod.POST, "/user/").hasAuthority("SCOPE_write")
-	            .anyRequest().authenticated())
-	          .oauth2ResourceServer(oauth2 -> oauth2.jwt());
-		}
-}
+//@Configuration
+//public class JWTSecurityConfig extends WebSecurityConfigurerAdapter {
+//
+//	 @Override
+//	    protected void configure(HttpSecurity http) throws Exception {
+//	        http
+//	          .authorizeRequests(authz -> authz
+//	            .antMatchers(HttpMethod.GET, "/user/**").hasAuthority("SCOPE_read")
+//	            .antMatchers(HttpMethod.POST, "/user/").hasAuthority("SCOPE_write")
+//	            .anyRequest().authenticated())
+//	          .oauth2ResourceServer(oauth2 -> oauth2.jwt());
+//		}
+//}
