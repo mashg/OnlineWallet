18c388781ed5df702b580e998072f29755f8ce58
AndryKovalenko <andry.kovalenko@gmail.com>
2021-02-26 09:41:41 +0200

    Comment dependecy

--- src/main/java/com/example/demo/config/JWTSecurityConfig.java
+++ src/main/java/com/example/demo/config/JWTSecurityConfig.java
@@ -2,8 +2,8 @@ package com.example.demo.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 //@Configuration
 //public class JWTSecurityConfig extends WebSecurityConfigurerAdapter {
